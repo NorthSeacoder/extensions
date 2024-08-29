c8a60e8418dee625454b1339615b6ecfc55c8e0d
mengpeng <pengmeng@yangqianguan.com>
2024-08-29 21:07:05 +0800

    build(bump): 调整下
    配置

--- libs/xmind-handler/bump.config.ts
+++ libs/xmind-handler/bump.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'bumpp'
 
 export default defineConfig({
   // ...options
-  tag:'@nsea/xmind-handler@',
-  files:['libs/xmind-handler/package.json'],
+  tag: '@nsea/xmind-handler@',
+  files: ['./libs/xmind-handler/package.json'],
 })
